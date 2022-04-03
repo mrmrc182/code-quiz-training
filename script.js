024355558fb2faeb0769852bfaa341c9c1709a60
Matthew Carlson <carlson.matt.r@gmail.com>
2022-04-02 17:51:12 -0700

    making a correct answer button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var answerTime = document.getElementById("time");
 var startButton = document.getElementById("startButton");
 var correctAnswer = document.querySelector("correct");
+var correctScore = document.querySelector("correctScore");
+var correctTotal = 0;
 var incorrectAnswer = document.querySelector("incorrect");
 var score = document.querySelector("score");
 var resetQuiz = document.getElementById("reset");
@@ -22,7 +24,12 @@ function clock() {
         }
         else {
             answerTime.textContent = "Loser";
+            setInterval;
         }
     }, 1000);
 }
-
+correctAnswer.addEventListener("click", function() {
+    correctScore = correctTotal + 1;
+    localStorage.setItem("correct", correctScore);
+    console.log(correctScore);
+});
